44d8cbdcf457aebf3351d50e150173b0110abe72
Sasi <sasikiran.tt3011@gmail.com>
2025-03-07 22:05:34 +0530

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import {AllRoutes} from './Routes/AllRoutes';
-
+import {Header} from './Components/Header';
 function App(){
   return (
     <div className="App">
+      <Header/>
         <AllRoutes/>
     </div>
   );
