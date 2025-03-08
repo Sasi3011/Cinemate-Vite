d8a8a7d4395b8319b5d42190be8a944e05c5622e
Sasi <sasikiran.tt3011@gmail.com>
2025-03-08 11:47:10 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import {AllRoutes} from './Routes/AllRoutes';
 import {Header} from './Components/Header';
+import {Footer} from './Components/Footer';
 function App(){
   return (
     <div className="App">
       <Header/>
         <AllRoutes/>
+      <Footer/>
     </div>
   );
 }
