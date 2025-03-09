9bbe8d91197c7fd099428728bbbb863455302bb4
Sasi <sasikiran.tt3011@gmail.com>
2025-03-09 09:56:11 +0530

    added movielist

--- src/Pages/MovieList.jsx
+++ src/Pages/MovieList.jsx
@@ -1,19 +1,27 @@
-import { useFetch } from "../hooks/useFetch";
-import { useTitle } from "../hooks/useTitle";
-import { Card } from "../Components";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import PageNotFoundImage from "../assets/images/pagenotfound.png"
+import { Button } from "../Components";
 
+export const PageNotFound = () => {
 
-export const MovieList = ({apiPath, title}) => {
-  const { data: movies } = useFetch(apiPath);
-  useTitle(title);
+  useEffect(() => {
+    document.title = `Page Not Found / Cinemate`;
+  });
 
   return (
     <main>
-      <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start flex-wrap other:justify-evenly">       
-          { movies.map((movie) => (
-            <Card key={movie.id} movie={movie} />
-          )) }          
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my-4">
+          <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white">404, Oops!</p>
+          <div className="max-w-lg">
+            <img className="rounded" src={PageNotFoundImage} alt="404 Page Not Found" />
+          </div>
+        </div>
+        <div className="flex justify-center my-4">
+          <Link to="/">
+            <Button>Back To Cinemate</Button>
+          </Link>          
         </div>
       </section>
     </main>
